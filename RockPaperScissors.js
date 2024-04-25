b417dc65c68e2da291457a4a67f0d7da8c8c0dc1
alanzhang03 <alan.s.zhang@gmail.com>
2024-04-24 22:43:50 -0400

    new

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -4,35 +4,18 @@
 
 function getComputerChoice(){
     let compChoice = Math.floor(Math.random() * 3);
-
-    if(compChoice = 0){
-        console.log("Computer chooses rock")
-    }else if(compChoice = 1){
-        console.log("Computer choice is paper");
-
-    }
-    else if(compChoice = 2){
-        console.log("Computer choice is scissors");
-    }
-    else{
-        console.log("error!");
-    }
     return compChoice;
 }
 
 function getHumanChoice(){
-    let humChoice = prompt("Choose option 0(rock), option 1(paper), or option 2(scissors)");
-    if(humChoice = 0){
-        console.log("You have choosen rock");
-    }else if(humChoice = 1){
-        console.log("You have choosen paper");
-    }else if(humChoice = 2){
-        console.log("You have choosen scissors");
+    let humanChoice = Number(prompt("Pick option 0, 1 or 2"));
+    if(humanChoice != '0' || humanChoice != '1' || humanChoice != '2'){
+        console.log("Please try again!");
     }
-    
+
+    return humanChoice;
 }
 
 
-
-
+console.log(getHumanChoice());
 console.log(getComputerChoice());
